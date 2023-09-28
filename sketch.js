0a3562280d09c894db4cd9a364c6a13da8ed0dc4
Malina <borosmalina@gmail.com>
2023-09-28 16:22:21 +0300

    Save button

--- sketch.js
+++ sketch.js
@@ -2,22 +2,26 @@ let myColor;
 
 function setup () {
   createCanvas(1200, 460);
-  background(255);
-  
+  background(255); 
   myColor = color(random(255), random(255), random(255));
+  select('#changeBG').html(myColor.toString('#rrggbb'));
 }
 
 function changeBG () {
   myColor = color(random(255), random(255), random(255));
-  select(".myButton").style('color', myColor);
-  select(".myButton").html(myColor.toString('#rrggbb'));
+  select('#changeBG').style('color', myColor);
+  select('#changeBG').html(myColor.toString('#rrggbb'));
   navigator.clipboard.writeText(myColor.toString('#rrggbb'));
-  alert('Color ' + myColor.toString('#rrggbb') + ' copied to clipboard!');
+
+  //alert('Color ' + myColor.toString('#rrggbb') + ' copied to clipboard!');
+}
+
+function saveImage(){
+  saveCanvas('myBanner', 'png');
 }
 
 function draw () {
   // Background color is changed every button press
   background(myColor);
 
- 
 }
