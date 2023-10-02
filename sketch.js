42ce96f21e8a21e7f35d64e3429fcf0c05c8b5e4
Malina <borosmalina@gmail.com>
2023-10-02 17:04:09 +0300

    Added clock

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let myColor;
-let emojiInput = "👍";
+let emojiInput = "🍉"; //"👍";
 
 function setup() {
+  angleMode(DEGREES);
   createCanvas(1200, 460);
   background(255);
   myColor = color(random(255), random(255), random(255));
@@ -22,9 +23,57 @@ function saveImage() {
 }
 
 function draw() {
-  // Background color is changed every button press
   background(myColor);
   pattern();
+
+  
+
+  translate(600,230);
+  rotate(-90);
+  
+  fill(180, 10, 65);
+  circle(0,0,350);
+
+  let hr = hour();
+  let mn = minute();
+  let sec = second();
+
+  strokeWeight(10);
+  stroke (0, 200, 50);
+
+  fill(255, 20, 100);
+  let secondAngle = map(sec, 0, 60, 0, 360);
+  arc(0, 0, 300, 300, 0, secondAngle);
+
+  fill(255, 80, 100);
+  let minuteAngle = map(mn, 0, 60, 0, 360) //+ map(secondAngle, 0, 360, 0, 6);
+  arc(0, 0, 250, 250, 0, minuteAngle);
+
+  fill(255, 110, 100);
+  let hourAngle = map(hr % 12, 0, 12, 0, 360) //+ map(minuteAngle, 0, 360, 0, 30);
+  arc(0, 0, 200, 200, 0, hourAngle);
+
+
+  push();
+  rotate(secondAngle);
+  stroke(0);
+  drawingContext.setLineDash([1,15]);
+  line(0,0,120,0);
+  pop();
+
+  push();
+  rotate(minuteAngle);
+  stroke(0);
+  line(0,0,110,0);
+  pop();
+
+  push();
+  rotate(hourAngle);
+  stroke(0);
+  line(0,0,80,0);
+  pop();
+
+
 }
 
 function pattern() {
@@ -46,6 +95,6 @@ function checkList(){
  // select("#divStuff").hide();
   print('ok');
   for (let indexBox = 0; indexBox < 10; indexBox++) {
-    createCheckbox("test", false);
+    createCheckbox("test", false).addClass("cringe");
   }
 }
