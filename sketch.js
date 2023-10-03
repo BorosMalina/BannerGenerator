22d0479ff231dfb0acb29a5d09b38d58238b908d
Malina <borosmalina@gmail.com>
2023-10-03 15:20:05 +0300

    Cleaned up the banner generator

--- sketch.js
+++ sketch.js
@@ -25,62 +25,6 @@ function saveImage() {
 function draw() {
   background(myColor);
   pattern();
-
-  translate(600,230);
-  rotate(-90);
-  
-  push();
-  fill(180, 10, 65);
-  stroke(0, 100, 50);
-  strokeWeight(20);
-  circle(0,0,350);
-  pop();
-
-  let hr = hour();
-  let mn = minute();
-  let sec = second();
-
-
-  strokeWeight(6);
-  stroke (0, 200, 50);
-  fill(255, 20, 100);
-  let secondAngle = map(sec, 0, 60, 0, 360);
-  arc(0, 0, 300, 300, 0, secondAngle);
-
-  strokeWeight(8);
-  fill(255, 80, 100);
-  let minuteAngle = map(mn, 0, 60, 0, 360) //+ map(secondAngle, 0, 360, 0, 6);
-  arc(0, 0, 250, 250, 0, minuteAngle);
-
-  strokeWeight(10);
-  fill(255, 110, 100);
-  let hourAngle = map(hr % 12, 0, 12, 0, 360) //+ map(minuteAngle, 0, 360, 0, 30);
-  arc(0, 0, 200, 200, 0, hourAngle);
-
-  
-  push();
-  rotate(secondAngle);
-  stroke(0);
-  drawingContext.setLineDash([1,15]);
-  line(0,0,120,0);
-  pop();
-
-  push();
-  rotate(minuteAngle);
-  stroke(0);
-  line(0,0,110,0);
-  pop();
-
-  push();
-  rotate(hourAngle);
-  stroke(0);
-  line(0,0,80,0);
-  pop();
-
-  rotate(secondAngle+secondAngle);
-  fill(255);
-  noStroke();
-  text("Melon Clock", -90, 0);
  
 }
 
